fd2c8f1f6f1b4c6e73063dada22ae02003948590
JohnMigwi <johnmigwi101@gmail.com>
2025-07-31 03:52:37 -0700

    literals

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 let firstname ="john";
 let lastname ="migwi";
+let num1= 100;
+let num2= 23;
 
-const fullname= `${firstname} ${lastname}`
+const fullname= `age is:${num1 - num2} name is:${firstname} ${lastname}`
 console.log(fullname);

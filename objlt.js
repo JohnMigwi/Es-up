56160e755f14f4a39737056dabd1bef88252fd69
JohnMigwi <johnmigwi101@gmail.com>
2025-08-05 10:27:56 -0700

    tmepl

--- objlt.js
+++ objlt.js
@@ -1,6 +1,8 @@
 const adressMaker = (city,state) => {
-    const newAdress = {city:city, state:state};
+    const newAdress = {city, state:};
     console.log(newAdress);
 }
 
 adressMaker("Nairobi", "Rongai");
+
+
